904b3204344b94ca40f8d35174081e50298788bc
TobbyMax <ageev.maxim2003@gmail.com>
2024-03-30 21:17:57 +0300

    fix: docker-compose

---
+++ migrations/tables.up.sql
@@ -0,0 +1,52 @@
+CREATE SCHEMA auth;
+CREATE SCHEMA profiles;
+
+CREATE TABLE auth.users
+(
+    id         uuid,
+    email      TEXT UNIQUE NOT NULL,
+    password   TEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE TYPE HABIT AS ENUM ('no', 'sometimes', 'yes', 'prefer not to say');
+CREATE TYPE INTENTION AS ENUM (
+    'life partner',
+    'long-term relationship',
+    'short-term relationship',
+    'friendship',
+    'figuring it out',
+    'prefer not to say'
+    );
+CREATE TYPE GENDER AS ENUM ('man', 'woman');
+CREATE TYPE PREFERRED_PARTNER AS ENUM ('men', 'women', 'anyone');
+CREATE TYPE FAMILY_PLANS AS ENUM ('don''t want children', 'want children', 'open to children', 'not sure yet', 'prefer not to say');
+
+CREATE TABLE profiles.profiles
+(
+    user_id           uuid,
+    first_name        TEXT,
+    last_name         TEXT,
+    birth_date        DATE,
+    sex               GENDER,
+    preferred_partner PREFERRED_PARTNER,
+    intention         INTENTION,
+    height            INTEGER,
+    has_children      BOOLEAN,
+    family_plans      TEXT,
+    location          TEXT,
+    drinks_alcohol    HABIT,
+    smokes HABIT,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE profiles.prompts
+(
+    id uuid,
+    user_id uuid,
+    position INTEGER,
+    question TEXT,
+    answer TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES profiles (user_id)
+);
