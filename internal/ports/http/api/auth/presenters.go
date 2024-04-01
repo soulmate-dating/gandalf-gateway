9976ea265b4f657c6afbc646809ab7cacd019321
TobbyMax <ageev.maxim2003@gmail.com>
2024-04-01 20:57:54 +0300

    fix: TIN-90 return tokens in json

--- internal/ports/http/api/auth/presenters.go
+++ internal/ports/http/api/auth/presenters.go
@@ -1,16 +1,22 @@
 package auth
 
+import "github.com/soulmate-dating/gandalf-gateway/internal/app/clients/auth"
+
 type loginRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,password"`
 }
 
 type User struct {
-	Id string `json:"id"`
+	Id           string `json:"id"`
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
 }
 
-func NewUser(id string) *User {
+func NewUser(r *auth.TokenResponse) *User {
 	return &User{
-		Id: id,
+		Id:           r.Id,
+		AccessToken:  r.AccessToken,
+		RefreshToken: r.RefreshToken,
 	}
 }
