c078f40ab601580d465172ed0f44d9d69886955f
TobbyMax <ageev.maxim2003@gmail.com>
2024-04-18 02:12:42 +0300

    fix: TIN-108 error string

--- internal/ports/http/response/base.go
+++ internal/ports/http/response/base.go
@@ -1,12 +1,13 @@
 package response
 
+// Response is a generic response structure.
 type Response struct {
-	Data  any   `json:"data"`
-	Error error `json:"error"`
+	Data  any `json:"data"`
+	Error any `json:"error"`
 }
 
 func Error(err error) Response {
-	return Response{Data: nil, Error: err}
+	return Response{Data: nil, Error: err.Error()}
 }
 
 func Success(data any) Response {
